55a3b7429816db22cd407696129673e27a55839f
negar anabestani <negar.anabestani@gmail.com>
2024-04-07 22:49:47 +0430

    fix url communicator

--- app/entity/Communicator.py
+++ app/entity/Communicator.py
@@ -67,10 +67,10 @@ class Communicator(object):
 
     def open_connection(self, url=None):
         fed_logger.info("connecting")
-        # if url is None:
-        #     url = config.mq_host
-        url = config.mq_host
-        self.url = url
+        if url is None:
+            url = config.mq_host
+        # url = config.mq_host
+        # self.url = url
 
         # url = config.mq_host
 
